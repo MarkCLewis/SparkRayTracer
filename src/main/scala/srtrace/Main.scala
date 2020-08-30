f6e1d42445bd095cb4a087329c583c37c71e97c7
Connor <43798687+ConnorWW@users.noreply.github.com>
2020-08-30 16:18:48 -0500

    gutted r3, put that gut in a main

---
+++ src/main/scala/srtrace/Main.scala
@@ -0,0 +1,36 @@
+package srtrace
+
+import java.awt.Graphics
+import java.awt.image.BufferedImage
+
+import javax.swing.{JFrame, WindowConstants}
+import org.apache.spark.{SparkConf, SparkContext}
+import swiftvis2.raytrace._
+
+object Main {
+  def main(args: Array[String]):Unit = {
+    val size = 600
+    val minX = -10
+    val maxX = 10
+    val conf = new SparkConf().setAppName("Renderer3").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("WARN")
+
+    val geom = sc.parallelize(GeometrySetup.randomGeometryActualArr(new scala.util.Random(System.currentTimeMillis), maxX, minX,20,10,10,-10,2, 5)) //actual geometries
+    val light: List[PointLight] = List(new PointLight(RTColor.White, Point(-2.0, 0.0, 2.0)))
+    val bimg: BufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB)
+    val view = GeometrySetup.standardView()
+    //  def render(geom: RDD[GeomSphere], light: List[PointLight], bImg: BufferedImage, view: (Point, Point, Vect, Vect), size: Int, numRays:Int = 1, numPartitions:Int = 8, minX:Double, maxX:Double): Unit = {
+
+    Renderer3.render(sc, geom, light, bimg, view, size, 1, 8, minX, maxX)
+
+    val frame = new JFrame {
+      override def paint(g: Graphics): Unit = {
+        g.drawImage(bimg, 0, 0, null)
+      }
+    }
+    frame.setSize(size, size)
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+    frame.setVisible(true)
+  }
+}
