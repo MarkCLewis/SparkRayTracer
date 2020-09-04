36d85c3bc28020c150b0d98d1a3267e76699d214
Erica Cater <ericacater@Ericas-Air.attlocal.net>
2020-09-04 12:20:08 -0500

    Testing 30 files better

--- src/main/scala/srtrace/EricasTestingFile.scala
+++ src/main/scala/srtrace/EricasTestingFile.scala
@@ -33,19 +33,22 @@ object EricasTestingFile {
     val sc = new SparkContext(conf)
     sc.setLogLevel("WARN")
 
-    val size = 1000
-    val minX = -150
-    val maxX = 150
-    val numPartitions = 10
-    val bimg: BufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB)
-    val light: List[PointLight] = List(new PointLight(RTColor.White, Point(-2.0, 0.0, 2.0)))
-    val view = GeometrySetup.standardView()
-    val offsets = Array[Int](0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29).map(i => (i*2.0e-5,0.0))
 
     val cartAndRadNumbers = Array[Int](5000, 5001, 5002, 5003, 5004, 5005, 5006, 5007, 5008, 5009, 5010, 5011, 
      5012, 5013, 5014, 5015, 5016, 5017, 5018, 5019, 5020, 5021, 5022, 5023, 5024, 5025, 5026, 5027, 5028, 5029)//, 
      //6000, 6001, 6002, 6003, 6004, 6005, 6006, 6007, 6008, 6009, 6010, 6011, 6012, 6013, 6014, 6015, 6016, 6017,
      //6018, 6019, 6020, 6021, 6022, 6023, 6024, 6025,6026, 6027, 6028, 6029)
+
+    val size = 1000
+    val minX = -150
+    val maxX = 150
+    val numPartitions = cartAndRadNumbers.length
+    val bimg: BufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB)
+    val light: List[PointLight] = List(new PointLight(RTColor.White, Point(-2.0, 0.0, 2.0)))
+    val view = GeometrySetup.standardView()
+    val offsets = Array[Int](0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29).map(i => (i*2.0e-5,0.0))
+
+    
 
     //val offsets = Array[(Double, Double)]((0,0), (2.0e-5, 0), (-2.0e-5, 0), (2*2.0e-5, 0), (-2*2.0e-5, 0), (3*2.0e-5, 0), (-3*2.0e-5, 0), (4*2.0e-5, 0), (-4*2.0e-5, 0), (5*2.0e-5, 0))
         // (0, 2.0e-5), (0, -2.0e-5), (0, 2*2.0e-5), (0, -2*2.0e-5)), 
@@ -58,16 +61,16 @@ object EricasTestingFile {
 
 
 
-    println(divisionOfFiles(sc, numPartitions, cartAndRadNumbers).collect().toList)
+    // println(divisionOfFiles(sc, numPartitions, cartAndRadNumbers).collect().toList)
 
 
     //Creates RDD[(Int,(Int, Double, Double))] with partition #, CartAndRad File #, offx, and offy
-    println(giveOffsets(sc, divisionOfFiles(sc, numPartitions, cartAndRadNumbers), offsets).collect().toList)
+    // println(giveOffsets(sc, divisionOfFiles(sc, numPartitions, cartAndRadNumbers), offsets).collect().toList)
 
 
     //Map to RDD[(Int, KDTreeGeometry)]
 
-    println(createKDTrees(sc, giveOffsets(sc, divisionOfFiles(sc, numPartitions, cartAndRadNumbers), offsets)).count())
+    // println(createKDTrees(sc, giveOffsets(sc, divisionOfFiles(sc, numPartitions, cartAndRadNumbers), offsets)).count())
 
 
     // var kd = createKDTrees(giveOffsets(divisionOfFiles(8, cartAndRadNumbers)))
