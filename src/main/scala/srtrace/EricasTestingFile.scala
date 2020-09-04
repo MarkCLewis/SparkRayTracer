b10f285c8e862d0cf7f06ce55cd6d474dde3aa22
Erica Cater <ericacater@Ericas-Air.attlocal.net>
2020-09-03 19:50:55 -0500

    ETF Producing Image Now

--- src/main/scala/srtrace/EricasTestingFile.scala
+++ src/main/scala/srtrace/EricasTestingFile.scala
@@ -15,16 +15,16 @@ object EricasTestingFile {
   def divisionOfFiles(sc: SparkContext, partitionNum: Int, cartAndRadNumbersArray: Array[Int]): RDD[(Int, Int)] = {
       val ret = Array.fill(cartAndRadNumbersArray.length)((0,0))
       for (i <- cartAndRadNumbersArray.indices) yield {
-          ret(i) = (i % partitionNum, cartAndRadNumbersArray(i))
+          ret(i) = (i % partitionNum-1, cartAndRadNumbersArray(i))
       }
       sc.parallelize(ret)
   }
 
-  def giveOffsets(r: RDD[(Int, Int)], offsets: Array[(Double, Double)]) : RDD[(Int,(Int, Double, Double))] = {
-      r.map( t => (t._1, (t._2, offsets(t._1)._1, offsets(t._1)._2)))
+  def giveOffsets(sc: SparkContext, r: RDD[(Int, Int)], offsetArray: Array[(Double, Double)]) : RDD[(Int,(Int, Double, Double))] = {
+      r.map( t => (t._1, (t._2, offsetArray(t._1)._1, offsetArray(t._1)._2)))
   }
 
-  def createKDTrees(r: RDD[(Int,(Int, Double, Double))]): RDD[(Int, KDTreeGeometry[BoundingSphere])] = {
+  def createKDTrees(sc: SparkContext, r: RDD[(Int,(Int, Double, Double))]): RDD[(Int, KDTreeGeometry[BoundingSphere])] = {
       r.mapValues(t => GeometrySetup.readRingWithOffset(t._1, t._2, t._3))
   }
 
@@ -40,17 +40,17 @@ object EricasTestingFile {
     val bimg: BufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB)
     val light: List[PointLight] = List(new PointLight(RTColor.White, Point(-2.0, 0.0, 2.0)))
     val view = GeometrySetup.standardView()
-    val offsets = Array[Int](0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29).map(i => (i*2.0e-5,0.0))
+    val offsets = Array[Int](0,1,2,3,4,5,6,7,8,9,10).map(i => (i*2.0e-5,0.0))//,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29).map(i => (i*2.0e-5,0.0))
 
-    val cartAndRadNumbers = Array[Int](5000, 5001, 5002, 5003, 5004, 5005, 5006, 5007, 5008, 5009, 5010, 5011, 
-     5012, 5013, 5014, 5015, 5016, 5017, 5018, 5019, 5020, 5021, 5022, 5023, 5024, 5025, 5026, 5027, 5028, 5029)//, 
+    val cartAndRadNumbers = Array[Int](5000, 5001, 5002, 5003, 5004, 5005, 5006, 5007, 5008, 5009)//, 5010, 5011, 
+     //5012, 5013, 5014, 5015, 5016, 5017, 5018, 5019, 5020, 5021, 5022, 5023, 5024, 5025, 5026, 5027, 5028, 5029)//, 
      //6000, 6001, 6002, 6003, 6004, 6005, 6006, 6007, 6008, 6009, 6010, 6011, 6012, 6013, 6014, 6015, 6016, 6017,
      //6018, 6019, 6020, 6021, 6022, 6023, 6024, 6025,6026, 6027, 6028, 6029)
 
-    // val offsets = Array[(Double, Double)]((0,0), (2.0e-5, 0), (-2.0e-5, 0), (2*2.0e-5, 0), (-2*2.0e-5, 0), //5
-    //     (0, 2.0e-5), (0, -2.0e-5), (0, 2*2.0e-5))//, (0, -2*2.0e-5))
+    //val offsets = Array[(Double, Double)]((0,0), (2.0e-5, 0), (-2.0e-5, 0), (2*2.0e-5, 0), (-2*2.0e-5, 0), (3*2.0e-5, 0), (-3*2.0e-5, 0), (4*2.0e-5, 0), (-4*2.0e-5, 0), (5*2.0e-5, 0))
+        // (0, 2.0e-5), (0, -2.0e-5), (0, 2*2.0e-5), (0, -2*2.0e-5)), 
 
-    val geom = createKDTrees(giveOffsets(divisionOfFiles(sc, numPartitions, cartAndRadNumbers), offsets))//.collect()//sc.parallelize(GeometrySetup.randomGeometryActualArr(new scala.util.Random(System.currentTimeMillis), maxX, minX,20,10,10,-10,2, 5)) //actual geometries
+    val geom = createKDTrees(sc, giveOffsets(sc, divisionOfFiles(sc, numPartitions, cartAndRadNumbers), offsets))//.collect()//sc.parallelize(GeometrySetup.randomGeometryActualArr(new scala.util.Random(System.currentTimeMillis), maxX, minX,20,10,10,-10,2, 5)) //actual geometries
     // val keyedGeoms: RDD[(Int, GeomSphere)] = geom.map(iGeom => ((iGeom.center.x - minX) / (maxX - minX) * numPartitions).toInt -> iGeom).repartition(numPartitions)
     // val groupedGeoms: RDD[(Int, Geometry)] = keyedGeoms.groupByKey().map { case (i, spheres) => i -> new KDTreeGeometry(spheres.toSeq) }
 
@@ -62,12 +62,12 @@ object EricasTestingFile {
 
 
     //Creates RDD[(Int,(Int, Double, Double))] with partition #, CartAndRad File #, offx, and offy
-    println(giveOffsets(divisionOfFiles(sc, numPartitions, cartAndRadNumbers), offsets).collect().toList)
+    println(giveOffsets(sc, divisionOfFiles(sc, numPartitions, cartAndRadNumbers), offsets).collect().toList)
 
 
     //Map to RDD[(Int, KDTreeGeometry)]
 
-    println(createKDTrees(giveOffsets(divisionOfFiles(sc, numPartitions, cartAndRadNumbers), offsets)).count())
+    println(createKDTrees(sc, giveOffsets(sc, divisionOfFiles(sc, numPartitions, cartAndRadNumbers), offsets)).count())
 
 
     // var kd = createKDTrees(giveOffsets(divisionOfFiles(8, cartAndRadNumbers)))
@@ -89,7 +89,7 @@ object EricasTestingFile {
         // val view = GeometrySetup.standardView()
         //  def render(geom: RDD[GeomSphere], light: List[PointLight], bImg: BufferedImage, view: (Point, Point, Vect, Vect), size: Int, numRays:Int = 1, numPartitions:Int = 8, minX:Double, maxX:Double): Unit = {
 
-        Renderer3.render(sc, geom, light, bimg, view, size)
+        Renderer3.render(sc, geom, light, bimg, view, size, 1, numPartitions)
 
         val frame = new JFrame {
             override def paint(g: Graphics): Unit = {
--- src/main/scala/srtrace/Renderer3.scala
+++ src/main/scala/srtrace/Renderer3.scala
@@ -11,7 +11,7 @@ import swiftvis2.raytrace._
 object Renderer3 {
 
   case class Pixel(x:Int, y:Int)
-  def render(sc: SparkContext, groupedGeoms: RDD[(Int, KDTreeGeometry[BoundingSphere])], light: List[PointLight], bImg: BufferedImage, view: (Point, Point, Vect, Vect), size: Int, numRays: Int = 1, numPartitions: Int = 8): Unit = {
+  def render(sc: SparkContext, groupedGeoms: RDD[(Int, KDTreeGeometry[BoundingSphere])], light: List[PointLight], bImg: BufferedImage, view: (Point, Point, Vect, Vect), size: Int, numRays: Int = 1, numPartitions: Int): Unit = {
 
     val img = new RTImage {
       def width = bImg.getWidth()
