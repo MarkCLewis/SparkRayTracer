e19a55b1249c997a1c8675b3cd0a209bf58f318a
Connor <43798687+ConnorWW@users.noreply.github.com>
2021-09-16 18:23:28 -0500

    switched to local running

--- src/main/scala/srtrace/photometry/PhotoTestFile.scala
+++ src/main/scala/srtrace/photometry/PhotoTestFile.scala
@@ -42,21 +42,21 @@ object PhotoTestFile extends App {
     6012, 6013, 6014, 6015, 6016, 6017, 6018, 6019, 6020, 6021, 6022, 6023,
     6024, 6025, 6026, 6027, 6028, 6029)
 
-  val kryoConf = new SparkConf().setAppName("photo")//.setMaster("local[*]")
+  val kryoConf = new SparkConf().setAppName("photo").setMaster("local[*]")
   
-  kryoConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+  //kryoConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
   //kryoConf.set("spark.dynamicAllocation.shuffleTracking.enabled", "true")
-  kryoConf.set("spark.executor.cores", "5")
+  //kryoConf.set("spark.executor.cores", "5")
   //kryoConf.set("spark.dynamicAllocation.enabled", "true")
   //kryoConf.set("spark.worker.memory", "16G")
   //kryoConf.set("spark.executor.cores", "31")
   //kryoConf.set("spark.executor.memory", "15G")
   //kryoConf.set("spark.dynamicAllocation.initialExecutors", "8")
   //kryoConf.set("spark.dynamicAllocation.maxExecutors", "8")
-  kryoConf.set("spark.kryoserializer.buffer", "2047M")
+  //kryoConf.set("spark.kryoserializer.buffer", "2047M")
   //kryoConf.set("spark.reducer.maxReqsInFlight", "1")
-  kryoConf.set("spark.shuffle.io.retryWait", "60s")
-  kryoConf.set("spark.shuffle.io.maxRetries", "10")
+  //kryoConf.set("spark.shuffle.io.retryWait", "60s")
+  //kryoConf.set("spark.shuffle.io.maxRetries", "10")
   
   val sc = new SparkContext(kryoConf)
   
@@ -73,7 +73,7 @@ object PhotoTestFile extends App {
   )
   sc.setLogLevel("WARN")
   sc.statusTracker.getExecutorInfos
-  val numPartitions = args(0).toInt
+  val numPartitions = 1//args(0).toInt
   
 
   val cartAndRadNumbers =
